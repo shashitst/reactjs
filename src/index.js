5d9434d636cfeb12738394fb3b49605be73035f7
shashank <shashanksharma00124@gmail.com>
2023-12-05 01:29:02 +0530

    added pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import about from "./pages/about";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+       
+          <Route index element={<Home />} />
+          
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<about />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const Root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
