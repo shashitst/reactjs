a3f15779093774f57448f1e723fdabbb1425b792
shashank <shashanksharma00124@gmail.com>
2024-01-10 12:37:20 +0530

    some more added

--- src/Back.js
+++ src/Back.js
@@ -3,7 +3,7 @@ export default function Back() {
    return (
       <div>
         
-         <br></br>
+        
          <div
             class = "image"
             style = {{
@@ -11,8 +11,10 @@ export default function Back() {
                width: "550px",
                backgroundImage:
                'url("https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/b2/1f/21/b21f21a8-e4f6-b7d2-1fec-8e5430273077/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png")',
-               backgroundSize: "contain",
+               backgroundSize: "cover",
                backgroundRepeat: "no-repeat",
+               
+               
             }}
          >
            
